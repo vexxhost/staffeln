b6c7126f715f53dc4888e85fd333be0f197a14d2
okozachenko <okozachenko@vexxhost.com>
2021-05-04 14:55:39 +0300

    register notification config

--- staffeln/conf/__init__.py
+++ staffeln/conf/__init__.py
@@ -3,6 +3,7 @@ from oslo_config import cfg
 from staffeln.conf import api
 from staffeln.conf import conductor
 from staffeln.conf import database
+from staffeln.conf import notify
 from staffeln.conf import paths
 
 CONF = cfg.CONF
@@ -10,4 +11,5 @@ CONF = cfg.CONF
 api.register_opts(CONF)
 conductor.register_opts(CONF)
 database.register_opts(CONF)
+notify.register_opts(CONF)
 paths.register_opts(CONF)
