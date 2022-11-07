15190e428f27a73527dddf1c887e262665bc2733
ricolin <rlin@vexxhost.com>
2022-11-07 08:31:31 +0800

    Add tooz_connection config option to database
    Add this option to support tooz driver.
    
    When using MySQL as tooz driver, this config can be like:
    `tooz_connection = "mysql://staffeln:password@localhost:3306/staffeln"`

--- staffeln/common/lock.py
+++ staffeln/common/lock.py
@@ -7,7 +7,7 @@ CONF = staffeln.conf.CONF
 
 class LockManager(object):
     def __init__(self, node_id=None):
-        self.db_url = CONF.database.connection
+        self.db_url = CONF.database.tooz_connection
         self.node_id = uuidutils.generate_uuid() if node_id is None else node_id
         # get_coordinator(backend_url, member_id)
         self.coordinator = coordination.get_coordinator(self.db_url, node_id)
